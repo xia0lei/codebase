a75eacd125259c9c67bdfd51352d5ce098c80463
XiaoLei Zhu <childmercy@163.com>
2022-05-06 20:55:21 +0800

    add trait Summary

--- rust-test/trait-test/src/main.rs
+++ rust-test/trait-test/src/main.rs
@@ -1,3 +1,12 @@
+use trait_test::Summary;
+use trait_test::Tweet;
+
 fn main() {
-    println!("Hello, world!");
+    let tweet = Tweet{
+        username:String::from("horse_ebooks"),
+        content:String::from("of course, as you probably already know, people"),
+        reply:false,
+        retweet:false
+    };
+    println!("new tweet:{}", tweet.summarize());
 }
